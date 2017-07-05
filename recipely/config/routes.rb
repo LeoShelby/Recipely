7c449f3030279854814fcf0156aa87852bf6b0ea
LeoShelby <leo-dp-95@hotmail.it>
2017-07-05 14:46:52 +0200

    Finish statica pages

--- recipely/config/routes.rb
+++ recipely/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/help'
+  root 'static_pages#home' #setta "home" come pagina iniziale
 
+  get 'static_pages/help'  #mappa le richieste per l’URL /static_pages/home alla home action nel StaticPages controller, la route deve rispondere a una GET request
+  get 'static_pages/about'
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
