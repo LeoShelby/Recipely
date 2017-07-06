b4d016eb04e68516b3e41b36ffdac7bead0a4bcd
LeoShelby <leo-dp-95@hotmail.it>
2017-07-06 23:22:21 +0200

    boo

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   #help_url -> 'http://www.example.com/help
   #così è possibile ad esempio utilizzare "help_path" nei link delle view
   
-  get 'signup' => 'users#new'
+  #get 'signup' => 'users#new'
   
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
