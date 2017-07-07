e2606b0da9e0f676b8349c13f6219cf9c55aea30
anjelo <catalaniangelo@gmail.com>
2017-07-07 18:13:32 +0200

    sposte nel branch test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,39 +1,14 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  def setup
-    @base_title=" Rails tutorial"
-  end
-  
-  test "should get root" do
-    get root_url
-    assert_response :success
-   
-  end
-  
-  
-  test "should get help" do
-    get help_url
-    assert_response :success
-    
-  end
-
-  test "should get about" do
-    get about_url
-    assert_response :success
-    
-  end
-
   test "should get home" do
-    get root_url
+    get root_path
     assert_response :success
-    
   end
- 
-  test "should get contact" do
-    get contact_url
+
+  test "should get help" do
+    get help_path
     assert_response :success
-   
   end
 
 end
