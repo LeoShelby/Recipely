7ea1273fe6edfae9625b9ad834cf9dc917383751
anjelo <catalaniangelo@gmail.com>
2017-07-11 03:44:31 +0200

    css mexxo fatto

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -11,8 +11,8 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     get user_path(@user)
     assert_template 'users/show'
     assert_select 'title', full_title(@user.name)
-    assert_select 'h1', text: @user.name
-    assert_select 'h1>img.gravatar'
+    assert_select 'h6', text: "Welcome to the kitchen of  "+@user.name
+   
     assert_match @user.recipes.count.to_s, response.body
     assert_select 'div.pagination'
     @user.recipes.paginate(page: 1).each do |recipe|
