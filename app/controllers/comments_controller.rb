e885680b0b16a0dadd750c7c89a0655370d9f5da
LeoShelby <leo-dp-95@hotmail.it>
2017-07-12 14:42:56 +0200

    before destroy comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,8 @@
 class CommentsController < ApplicationController
-
+	before_action :logged_in_user , only: [:create,:destroy]
+	before_action :correct_user,   only: [:destroy]
+	
+	
 	def create
 	  @recipe = Recipe.find(params[:recipe_id])
 	  @comment = @recipe.comments.build(comment_params)
@@ -32,6 +35,14 @@ class CommentsController < ApplicationController
     private
 		def comment_params
 			params.require(:comment).permit(:recipe_id,:user_id,:content)
+		end
+		
+		def correct_user
+			@comment=current_user.comments.find_by(id: params[:id])
+			if @comment.nil?
+				redirect_to root_url 
+				flash[:danger]= "Can't delete the comment"
+			end
 		end
   
 end
