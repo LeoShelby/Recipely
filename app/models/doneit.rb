1475872ecdb92e377322e3b54f601320c3bc0cfd
LeoShelby <leo-dp-95@hotmail.it>
2017-07-13 17:54:17 +0200

    notifiche per done it

--- app/models/doneit.rb
+++ app/models/doneit.rb
@@ -1,4 +1,21 @@
 class Doneit < ApplicationRecord
+  after_create :create_notification
+	
   belongs_to :user
   belongs_to :recipe
+  
+  validates :user_id,presence: true
+  validates :recipe_id, presence: true
+  
+  
+  private      
+	def create_notification
+		Notification.create(
+		  user:  recipe.user,
+		  notified_by:  user,
+		  type_not: "doneit",
+		  recipe: recipe,
+		  read: false
+		)
+	end
 end
