e9f2abbaf23ec839649642cdbbd4e1abc8208a8f
anjelo <catalaniangelo@gmail.com>
2017-07-16 20:15:11 +0200

    ppp

--- db/migrate/20170716154726_create_allergens.rb
+++ db/migrate/20170716154726_create_allergens.rb
@@ -1,7 +1,7 @@
 class CreateAllergens < ActiveRecord::Migration[5.0]
   def change
     create_table :allergens do |t|
-      t.string :type
+      t.string :type_allergen
 
       t.timestamps
     end
